d16ac17e92510ad80950cd8214cd68ea716419a7
Cassandra Smith <65835968+cassandrafsmith@users.noreply.github.com>
2022-06-26 00:00:13 -0500

    update expenses stateful list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,13 +5,15 @@ import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
-    const [selectedYear, setSelectedYear] = useState('');
+    const [selectedYear, setSelectedYear] = useState('All');
 
     const selectedYearHandler = (selection) =>{        
        setSelectedYear(selection);
     };
 
-    console.log(selectedYear);
+    // const filteredExpenses = props.data.filter((expense) => {
+    //    return expense.date.getFullYear().toString() === selectedYear;
+    // });     
 
     return(
         <div>            
@@ -19,27 +21,27 @@ const Expenses = (props) => {
                 <ExpensesFilter 
                     selected={selectedYear} 
                     onSelectYear={selectedYearHandler} 
-                />            
-                <ExpenseItem 
-                    title={props.data[0].title}
-                    amount={props.data[0].amount}
-                    date={props.data[0].date}
-                />
-                <ExpenseItem 
-                    title={props.data[1].title}
-                    amount={props.data[1].amount}
-                    date={props.data[1].date}
-                />
-                <ExpenseItem 
-                    title={props.data[2].title}
-                    amount={props.data[2].amount}
-                    date={props.data[2].date}
-                />
-                <ExpenseItem 
-                    title={props.data[3].title}
-                    amount={props.data[3].amount}
-                    date={props.data[3].date}
-                />
+                />  
+                {selectedYear ==='All' ?      
+                    props.data.map((expense) => (
+                        <ExpenseItem 
+                            key={expense.id}
+                            title={expense.title} 
+                            amount={expense.amount} 
+                            date={expense.date} 
+                        />
+                    )) :
+                    props.data.filter((expense) => ( 
+                        expense.date.getFullYear().toString() === selectedYear)
+                        ).map((expense) => (
+                        <ExpenseItem 
+                            key={expense.id}
+                            title={expense.title} 
+                            amount={expense.amount} 
+                            date={expense.date} 
+                        />
+                    ))    
+                }               
             </Card>  
         </div>              
     )
